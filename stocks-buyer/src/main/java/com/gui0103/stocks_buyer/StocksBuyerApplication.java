e472c272261e8fb55889e8dd3ae3cdeb6194631f
gui0103 <gui03costa@gmail.com>
2025-08-12 16:24:39 -0300

    Starting Spring

---
+++ stocks-buyer/src/main/java/com/gui0103/stocks_buyer/StocksBuyerApplication.java
@@ -0,0 +1,13 @@
+package com.gui0103.stocks_buyer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocksBuyerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocksBuyerApplication.class, args);
+	}
+
+}
---
+++ stocks-buyer/src/test/java/com/gui0103/stocks_buyer/StocksBuyerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gui0103.stocks_buyer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocksBuyerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
